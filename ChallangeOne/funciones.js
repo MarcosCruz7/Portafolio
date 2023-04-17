06744f54b412b14a01b7f8e8576009f6460ff0d0
Marcos Cruz <cruzvm@outlook.com>
2023-04-17 14:43:19 -0600

    Creando index y funciones

---
+++ ChallangeOne/funciones.js
@@ -0,0 +1,85 @@
+function obtener() {
+    let valorTextarea = document.getElementById("texto").value;
+    return valorTextarea;
+}
+
+
+
+function encriptar(text){
+    
+    let palabras = text.split(/[ ,;:]+/);
+	console.log(palabras);
+
+	let letras = [];
+	let palabraAux = "";
+	let encriptadas = [];
+
+	for (let i = 0; i < palabras.length; i++) {
+  		const palabra = palabras[i];
+  		console.log(palabra);
+  		for (let j = 0; j < palabra.length; j++) {
+    		const letra = palabra[j];
+    		console.log(letra);
+    		console.log(palabraAux);
+
+    		if(letra == "a"){
+    			//letras.push("ai");
+    			palabraAux = palabraAux + "ai";
+    		}else
+    		 if(letra == "e"){
+    			//letras.push("enter");
+    			palabraAux = palabraAux + "enter";
+    		}
+    		if(letra == "i"){
+    			//letras.push("imes");
+    			palabraAux = palabraAux + "imes";
+    		}
+    		if(letra == "o"){
+    			//letras.push("ober");
+    			palabraAux = palabraAux + "ober";
+    		}
+    		if(letra == "u"){
+    			//letras.push("ufat");
+    			palabraAux = palabraAux + "ufat";
+    		}
+    		if(letra != "a" && letra != "e" && letra != "i" &&
+    			letra != "o" && letra != "u"){
+    		palabraAux = palabraAux + letra;
+    		letras.push(letra);
+    		
+    		}
+    	}
+   		    		
+    		console.log(encriptadas);
+    		encriptadas.push(palabraAux);
+    		palabraAux = "";
+    		//Letras = [0];
+  		}
+
+		console.log(letras);
+		console.log(encriptadas);
+
+		let textoEncryptado = "";
+
+		for(let indice = 0; indice < encriptadas.length; indice++){
+			textoEncryptado = textoEncryptado + " " + encriptadas[indice];
+		}
+
+        alert(textoEncryptado);
+}
+
+
+
+let btnEncriptar = document.getElementById("btnEncriptar");
+btnEncriptar.addEventListener("click", function() {
+    let encrip = encriptar(obtener());
+    console.log(encrip);
+    // Aquí puedes hacer algo con el valor de texto, como encriptarlo
+});
+	
+
+		
+
+
+
+	
